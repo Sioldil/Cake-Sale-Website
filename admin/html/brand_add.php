44dc8441a0ef34d553e51b22f8b8cad49bcfaf77
NguyenDinhHung2k1 <nguyenhung16052001@gmail.com>
2023-03-09 15:52:22 +0700

    update image brands

--- admin/html/brand_add.php
+++ admin/html/brand_add.php
@@ -2,15 +2,26 @@
 include($_SERVER["DOCUMENT_ROOT"] . '/admin/inc/header(not template).php');
 include($_SERVER['DOCUMENT_ROOT'] . "/database/connect.php");
 
-if (isset($_POST['name'])) {
-  $name = $_POST['name'];
-  $query = "INSERT INTO Brands(Name) Value('$name')";
-  $data = mysqli_query($conn, $query);
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-  if ($data) {
-    header("location: brand_list.php");
-  } else {
-    echo "Xảy ra lỗi khi thêm mới";
+  if(empty($_POST['name']) && empty($_FILES['photo'])){
+    echo "Dữ liệu rỗng";
+  }else{
+    $name = $_POST['name'];
+    if(isset($_FILES['photo'])){
+      $file_name =  time() . '_' .$_FILES['photo']['name'];
+      $file_tmp = $_FILES['photo']['tmp_name'];
+      move_uploaded_file($file_tmp,"..//uploads//".$file_name);
+    
+      $query = "INSERT INTO Brands(Name,Image) Value('$name','$file_name')";
+      $data = mysqli_query($conn, $query);
+    
+      if ($data) {
+        header("location: brand_list.php");
+      } else {
+        echo "Xảy ra lỗi khi thêm mới";
+      }
+    }
   }
 }
 
@@ -25,16 +36,16 @@ if (isset($_POST['name'])) {
       <div class="col-xl">
         <div class="card mb-4">
           <div class="card-body">
-            <form method="POST" enctype="multipart/form">
+            <form action="brand_add.php" method="POST" enctype="multipart/form-data">
               <div class="mb-3">
                 <label class="form-label" for="name">Tên thương hiệu</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Bánh kem Le Castella" />
+                <input type="text" class="form-control" id="name" name="name" placeholder="Bánh kem Le Castella" required />
               </div>
               <div class="mb-3">
-                <label class="form-label" for="image">Hình ảnh</label>
-                <input type="file" class="form-control" id="image" name="image" />
+                <label class="form-label">Hình ảnh</label>
+                <input type="file" class="form-control" name="photo" required/>
               </div>
-              <button type="submit" name='submit' class="btn btn-primary">Lưu</button>
+              <button type="submit" class="btn btn-primary">Lưu</button>
             </form>
           </div>
         </div>
---
+++ admin/html/contact_add.php
@@ -0,0 +1,60 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"] . '/admin/inc/header(not template).php');
+include($_SERVER['DOCUMENT_ROOT'] . "/database/connect.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message'])) {
+        echo "Dữ liệu rỗng";
+    } else {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        $query = "INSERT INTO Contacts (UserName,Email,Message) Value('$name','$email','$message')";
+        $data = mysqli_query($conn, $query);
+
+        if ($data) {
+            header("location: contact_list.php");
+        } else {
+            echo "Xảy ra lỗi khi thêm mới";
+        }
+    }
+}
+
+?>
+
+<div class="content-wrapper">
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"> Thêm Phản Hồi</h4>
+        <!-- Basic Layout -->
+        <div class="row">
+            <div class="col-xl">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <form action="contact_add.php" method="POST">
+                            <div class="mb-3">
+                                <label class="form-label" for="name">Tên người dùng</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Nguyễn Văn A" required />
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="email">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="abc@gmail.com" required />
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="message">Nội dung</label>
+                                <input type="text" class="form-control" id="message" name="message" placeholder="Nội dung" required />
+                            </div>
+                            <button type="submit" class="btn btn-primary">Gửi</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/admin/inc/footer.php");
+?>
